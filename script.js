ef6f24e5b434ba91bd119b46e0167a27878cc432
Sayed-Anawer <sayedanawer99@gmail.com>
2025-04-14 04:05:00 +0600

    Script added for close and open the menu

---
+++ script.js
@@ -0,0 +1,18 @@
+const openMenuButton = document.querySelector("#menu-open-button");
+const closeMenuButton = document.querySelector("#menu-close-button");
+
+openMenuButton.addEventListener('click', ()=>{
+    document.body.classList.toggle("show-mobile-menu")
+})
+closeMenuButton.addEventListener('click', ()=>{
+
+    document.body.classList.remove("show-mobile-menu")
+})
+
+/** 
+ *  Another way to close menu
+ */ 
+
+// closeMenuButton.addEventListener('click',()=>{
+//     openMenuButton.click()
+// })
