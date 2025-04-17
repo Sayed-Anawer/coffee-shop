1a210c0f3fd9b37abedce0ae40ce045fbb33a3a6
Sayed-Anawer <sayedanawer99@gmail.com>
2025-04-17 11:23:56 +0600

    Testimonial section styles and swiper js added

--- script.js
+++ script.js
@@ -17,3 +17,37 @@ closeMenuButton.addEventListener("click", () => {
 //     openMenuButton.click()
 // })
 
+// Swiper JS
+const swiper = new Swiper(".slider-wrapper", {
+  // Optional parameters
+
+  loop: true,
+  spaceBetween: 20,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+    
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  // Responsive Breakpoints
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
